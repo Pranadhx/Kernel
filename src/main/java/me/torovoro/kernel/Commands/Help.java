751a48cbf801e6bb124d36ad617c23d7b020986c
Pranadhx <pranadha101@gmail.com>
2022-12-07 16:05:50 +1100

    implemented a config reload command

--- src/main/java/me/torovoro/kernel/Commands/Help.java
+++ src/main/java/me/torovoro/kernel/Commands/Help.java
@@ -1,13 +1,18 @@
 package me.torovoro.kernel.Commands;
 
+import me.torovoro.kernel.Kernel;
 import me.torovoro.kernel.Utils.ColorUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 public class Help implements CommandExecutor {
+
+    Plugin plugin = Kernel.getPlugin(Kernel.class);
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if(command.getName().equalsIgnoreCase("kernel")) {
@@ -15,6 +20,9 @@ public class Help implements CommandExecutor {
                 Player player = (Player) sender;
                 if(args.length < 1) {
                     player.sendMessage(ColorUtils.translateColorCodes("&#968863&l* &7Running &#fce9b6&lK&#fde4ae&lE&#fddea7&lR&#fed99f&lN&#fed398&lE&#ffce90&lL &#ccc695v1.0.\n&#968863&l* &7Use &#ccc695/kernel help &7to view all commands."));
+                } else if(args[0].equals("reload")) {
+                    plugin.reloadConfig();
+                    player.sendMessage(ColorUtils.translateColorCodes("&#968863&l* &#fce9b6Config &7has been &#fce9b6reloaded!"));
                 } else {
                     player.sendMessage(ColorUtils.translateColorCodes("&8&m                                                                        "));
                     player.sendMessage(ColorUtils.translateColorCodes("&a                           &#c6b78f&lC&#c3b28a&lO&#c0ae86&lM&#bda981&lM&#baa57d&lA&#b7a078&lN&#b49c74&lD&#b1976f&lS &#ae936b&lL&#ab8e66&lI&#a88a62&lS&#a5855d&lT"));
