d79bef8fbe7985a9d69a3345cfd420fa505ad02d
Pranadhx <pranadha101@gmail.com>
2022-12-07 23:40:40 +1100

    tntrod tick values are editable in config now!

--- src/main/java/me/torovoro/kernel/Events/TNTRod.java
+++ src/main/java/me/torovoro/kernel/Events/TNTRod.java
@@ -1,5 +1,6 @@
 package me.torovoro.kernel.Events;
 
+import me.torovoro.kernel.Kernel;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -8,11 +9,18 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
 
 public class TNTRod implements Listener {
+
+    Plugin plugin = Kernel.getPlugin(Kernel.class);
+
     @EventHandler
     public void RodRightClick(PlayerInteractEvent event) {
+
+        int tnt_explosion_ticks = plugin.getConfig().getInt("tnt_explosion_ticks");
+
         Player player = event.getPlayer();
         if(!event.getAction().isRightClick() | !event.getHand().equals(EquipmentSlot.HAND))
             return;
@@ -23,6 +31,7 @@ public class TNTRod implements Listener {
         // Grab player's head location and place a primed TNT
         Location location = player.getEyeLocation();
         TNTPrimed tnt = player.getWorld().spawn(location, TNTPrimed.class);
+        tnt.setFuseTicks(tnt_explosion_ticks);
         // Multiply direction from location by 2 and store as vector
         Vector v = location.getDirection().multiply(2);
         // Apply vector to primed TNT
